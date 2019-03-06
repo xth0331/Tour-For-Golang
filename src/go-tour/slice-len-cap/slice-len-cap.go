742959853e21d40a4884a84982b972afa166eed3
Xieth <xietianhao0331@gmail.com>
2019-03-07 00:44:24 +0800

    Test the slices

---
+++ src/go-tour/slice-len-cap/slice-len-cap.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	// 截取切片使其长度为 0
+	s = s[:1]
+	printSlice(s)
+
+	// 拓展其长度
+	s = s[:4]
+	printSlice(s)
+
+	// 舍弃前两个值
+	s = s[0:5]
+	printSlice(s)
+	
+	s = s[1:4]
+	printSlice(s)
+
+	s = s[:1]
+	printSlice(s)
+
+	s = s[2:3]
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
