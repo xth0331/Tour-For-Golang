3f7767efa4e0473652c08150f63f2e61b255d438
Xieth <xietianhao0331@gmail.com>
2019-03-10 18:01:36 +0800

    update

---
+++ src/go-tour/exercise-slices/exercise-slices.go
@@ -0,0 +1,21 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	a := make([][]uint8, dy)
+	for i := range a {
+		a[i] = make([]uint8, dx)
+	}
+
+	for i := range a {
+		for j := range a[i] {
+			a[i][j] = uint8(j ^ i)
+		}
+	}
+	return a
+}
+
+func main() {
+	pic.Show(Pic)
+}
