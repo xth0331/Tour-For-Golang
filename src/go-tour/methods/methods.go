78cfc199fe547ef164d50bd2cc4994dd7cd39855
Xieth <xietianhao0331@gmail.com>
2019-03-14 23:50:31 +0800

    update methods

---
+++ src/go-tour/methods/methods.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Vertex is ...
+type Vertex struct {
+	X, Y float64
+}
+
+// Abs is ...
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+}
