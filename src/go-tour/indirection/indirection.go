e54381786450f22b2283e7e8de11b3b5b2702320
Xieth <xietianhao0331@gmail.com>
2019-03-18 23:23:19 +0800

    update code

---
+++ src/go-tour/indirection/indirection.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Vertex is ...
+type Vertex struct {
+	X, Y float64
+}
+
+// Scale is ...
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+// ScaleFunc is ...
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+
+	p := &Vertex{4, 3}
+	p.Scale(3)
+	ScaleFunc(p, 8)
+
+	fmt.Println(v, p)
+}
