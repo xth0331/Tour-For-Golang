69b7e81e2dcc46e44f6d84cec1d3ba7a48a84144
Xieth <xietianhao0331@gmail.com>
2019-04-07 23:59:48 +0800

    add: gopl/echo01/main.go

---
+++ src/gopl/ch01/echo1/main.go
@@ -0,0 +1,16 @@
+// Echo1 prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, seq string
+	for i := 1; i < len(os.Args); i++ {
+		s += seq + os.Args[i]
+		seq = " "
+	}
+	fmt.Println(s)
+}
