26d33a21b5da7bf64463b62a8c13d99a0a5e1bec
Xieth <xietianhao0331@gmail.com>
2019-05-13 23:00:22 +0800

    update workspace

--- src/gopl/ch02/tempconv/tempconv.go
+++ src/gopl/ch02/tempconv/tempconv.go
@@ -1,10 +1,11 @@
 // Package tempconv performs Celsius and Fahrenheit temperature computations.
 package tempconv
+
 import "fmt"
 
 type Celsius float64    // 摄氏温度
 type Fahrenheit float64 // 华氏温度
-type Kelvins float64 // 开氏温度
+type Kelvins float64    // 开氏温度
 
 const (
 	AbsoluteZeroC Celsius = -273.15 // 绝对零度
@@ -12,6 +13,6 @@ const (
 	BoilingC      Celsius = 100     // 沸水温度
 )
 
-func (c Celsius） String() string { return fmt.Sprintf("%g°C", c) }
-func (f Fahrenheit） String() string { return fmt.Sprintf("%g°F"， f) }
-func (k Kelvins） String() string { return fmt.Sprintf("%g°K"， k) }
+func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvins) String() string    { return fmt.Sprintf("%g°K", k) }
