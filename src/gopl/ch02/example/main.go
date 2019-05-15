d099d63969c1fe90f09bdee1e094a0996082e793
Xieth <xietianhao0331@gmail.com>
2019-05-15 22:04:25 +0800

    Update: example

--- src/gopl/ch02/example/main.go
+++ src/gopl/ch02/example/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
-	f, err := os.Open("foo.txt")
-	fmt.Println(f, err)
+	o := 0666
+	fmt.Printf("%d %[1]o %#[1]o\n", o) // "438 666 0666"
+	x := int64(0xdeadbeef)
+	fmt.Printf("%d %[1]x %#[1]x %#[1]X\n", x)
+	ascii := 'a'
+	unicode := '国'
+	newline := '\n'
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)   // "97 a 'a'"
+	fmt.Printf("%d %[1]c %[1]q\n", unicode) // "22269 国 '国'"
+	fmt.Printf("%d %[1]q\n", newline)       // "10 '\n'"
 }
