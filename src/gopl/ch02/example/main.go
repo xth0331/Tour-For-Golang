b187f28945b2d3cc245bd895a0c75a2f041c8b87
Xieth <xietianhao0331@gmail.com>
2019-05-16 21:55:11 +0800

    Update: ch03

--- src/gopl/ch02/example/main.go
+++ src/gopl/ch02/example/main.go
@@ -4,15 +4,8 @@ import (
 	"fmt"
 )
 
+func itob(i int) bool { return i != 0 }
+
 func main() {
-	o := 0666
-	fmt.Printf("%d %[1]o %#[1]o\n", o) // "438 666 0666"
-	x := int64(0xdeadbeef)
-	fmt.Printf("%d %[1]x %#[1]x %#[1]X\n", x)
-	ascii := 'a'
-	unicode := '国'
-	newline := '\n'
-	fmt.Printf("%d %[1]c %[1]q\n", ascii)   // "97 a 'a'"
-	fmt.Printf("%d %[1]c %[1]q\n", unicode) // "22269 国 '国'"
-	fmt.Printf("%d %[1]q\n", newline)       // "10 '\n'"
+
 }
