f91f49237fbb67c45fdaa0ac632c054528e42f85
Xieth <xietianhao0331@gmail.com>
2019-06-15 14:55:47 +0800

    Fix: int

--- src/go_code/chapter04/demo04/main.go
+++ src/go_code/chapter04/demo04/main.go
@@ -10,12 +10,13 @@ func test() bool {
 	fmt.Println("test...")
 	return true
 }
+
 // 短路与， && ：如果第一个条件为false，则第二个条件不会判断，最终结果为false。
 // 短路或， || ： 如果第一个条件为true，则第二个条件不会判断，最终结果为true。
 func main() {
 
-	var i int = 10
-//	if i < 9 && test() {
+	var i = 10
+	//	if i < 9 && test() {
 	if i > 9 || test() {
 		fmt.Println("ok...")
 	}
