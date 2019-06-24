8b0186f6af7db8e95d56cc2e66edd780fe7c0cd4
Xieth <xietianhao0331@gmail.com>
2019-06-24 22:24:18 +0800

    Add：exec04

---
+++ src/go_code/chapter05/exec/exec04/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var age int
+	var month int
+
+	fmt.Println("请输入月份:")
+	fmt.Scanln(&month)
+
+	fmt.Println("年龄：")
+	fmt.Scanln(&age)
+
+	if month >= 4 && month <= 10 {
+		if age < 18 {
+			fmt.Println("旺季儿童半价票：30")
+		} else if age >= 18 && age <= 60 {
+			fmt.Println("旺季成人票：60")
+		} else {
+			fmt.Println("旺季老人票：20")
+		}
+	} else {
+		if age >= 18 && age <= 60 {
+			fmt.Println("淡季成人票：40")
+		} else {
+			fmt.Println("淡季半价票：20")
+		}
+	}
+}
