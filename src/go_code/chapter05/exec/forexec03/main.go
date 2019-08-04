359ffd5ecb19e927d0719c5ab60caf3e36504019
Xieth <xietianhao0331@gmail.com>
2019-08-04 17:31:11 +0800

    Update: main.go

--- src/go_code/chapter05/exec/forexec03/main.go
+++ src/go_code/chapter05/exec/forexec03/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+// i 表示层数
+// j 表示打印多少个星
+// k 表示*前面的空格数
+
 func main() {
 	var laynum int
 	fmt.Println("输入层数：")
@@ -13,7 +17,12 @@ func main() {
 			fmt.Printf(" ")
 		}
 		for j := 1; j <= 2*i-1; j++ {
-			fmt.Printf("*")
+			if j == 1 || j == 2*i-1 || i == laynum {
+				fmt.Printf("*")
+			} else {
+				fmt.Printf(" ")
+			}
+
 		}
 		fmt.Println()
 	}
